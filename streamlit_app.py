c0a2180472fe84907cccc97fd463f8dbea6ef733
inmetamartin <143177114+inmetamartin@users.noreply.github.com>
2023-09-11 11:33:54 +0200

    Update streamlit_app.py
    try catch on input

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,16 +22,19 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruitivice Fruit Advice')
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-# normalizes json to table 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# prints the dataframe to streamlit
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  streamlit.write('The user entered ', fruit_choice)
+  if not fruit_choice:
+    stremlit.error("Select a fruit to get information")
+  else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)    
+      # normalizes json to table 
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      # prints the dataframe to streamlit
+      streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 
 streamlit.stop()
