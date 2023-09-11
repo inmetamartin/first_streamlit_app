71eb2d2cc47f8c2f9c77205ddfda87ef1fe68d6c
inmetamartin <143177114+inmetamartin@users.noreply.github.com>
2023-09-11 10:59:31 +0200

    Update streamlit_app.py
    added streamlit stop and cleanup

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
-import pandas
+#import pandas
 import snowflake.connector
+import requests
+from urllib.error import URLError
 
 streamlit.title('My parents New Healthy Diner')
 
@@ -23,13 +25,16 @@ streamlit.header('Fruitivice Fruit Advice')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 # normalizes json to table 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # prints the dataframe to streamlit
 streamlit.dataframe(fruityvice_normalized)
+
+
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
