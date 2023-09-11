da85ac8fc4e37cd49854fed723f09c4e375a532a
inmetamartin <143177114+inmetamartin@users.noreply.github.com>
2023-09-11 15:29:32 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,8 @@ def insert_row_snowflake(new_fruit):
   streamlit.write("in inserst_row_snowflake ")
   try:
     with my_cnx.cursor() as my_cur:
-      my_cur.execute("insert into fruit_load_list(:new_fruit)")
+      sqlIns = 
+      #my_cur.execute("insert into fruit_load_list values(:new_fruit)")
       return "Thanks for adding "+ new_fruit
   except:
     streamlit.write("error in insert ")
